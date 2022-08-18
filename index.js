b3e9ea01f72c016be326b1bc15c2f2d0b4b34ffd
Nicholas Fernandez <Nickfer610@gmail.com>
2022-08-18 23:28:16 +0000

    progress i guess..

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const fs = require('fs');
+const read = require('./read');
+
 
 const selectFile = process.argv[2];
-fs.readFile(`./${selectFile}`, 'utf8', (err, data) => {
-  console.log(process.argv[2])
-  if (!err) {
-    console.log(`Text of ${selectFile}: ${data}`);
-  } else {
-    console.log('There was an error');
-  }
-});
+console.log(process.argv);
+
+
+// fs.readFile(`./${selectFile}`, 'utf8', (err, data) => {
+//   console.log(process.argv[2])
+//   if (!err) {
+//     console.log(`Text of ${selectFile}: ${data}`);
+//   } else {
+//     console.log('There was an error');
+//   }
+// });
---
+++ read.js
@@ -0,0 +1,8 @@
+fs.readFile(`./${selectFile}`, 'utf8', (err, data) => {
+  console.log(process.argv[2])
+  if (!err) {
+    console.log(`Text of ${selectFile}: ${data}`);
+  } else {
+    console.log('There was an error');
+  }
+});
